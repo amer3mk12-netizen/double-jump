acba5d8f897492d5c4defaca81d0b19bb1223356
amer3mk12-netizen <amer3mk12@gmail.com>
2026-09-01 13:11:35 +0300

    Update DoubleJump.java

--- src/main/java/com/amermk/doublejump/DoubleJump.java
+++ src/main/java/com/amermk/doublejump/DoubleJump.java
@@ -3,12 +3,9 @@ package com.amermk.doublejump;
 import net.fabricmc.api.ModInitializer;
 
 public class DoubleJump implements ModInitializer {
-
-    public static final String MOD_ID = "doublejump";
+    public static final String MOD_ID = "double-jump";
 
     @Override
     public void onInitialize() {
-        DoubleJumpPayload.register();
     }
 }
-
